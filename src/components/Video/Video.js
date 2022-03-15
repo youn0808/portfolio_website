ffc524184bd957a7783f8bb751a3a901ff4c2ccb
SeunghwanYoun <seunghwan@SeunghwYounsMBP.hitronhub.home>
2022-03-15 05:22:50 -0500

    fix video

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,34 +1,54 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
 
-const Video = () => {
-  const videoEl = useRef(null);
+// const Video = () => {
+//   const videoEl = useRef(null);
 
-  const attemptPlay = () => {
-    videoEl &&
-      videoEl.current &&
-      videoEl.current.play().catch((error) => {
-        console.error("Error attempting to play", error);
-      });
-  };
+//   const attemptPlay = () => {
+//     videoEl &&
+//       videoEl.current &&
+//       videoEl.current.play().catch((error) => {
+//         console.error("Error attempting to play", error);
+//       });
+//   };
 
-  useEffect(() => {
-    attemptPlay();
-  }, []);
+//   useEffect(() => {
+//     attemptPlay();
+//   }, []);
 
-  return (
-    <div>
-      <video
-        style={{ maxWidth: "97%" }}
-        playsInline
-        loop
-        muted
-        controls
-        alt="All the devices"
-        src="/movie_app_demo.mp4"
-        ref={videoEl}
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <video
+//         style={{ maxWidth: "97%" }}
+//         playsInline
+//         loop
+//         muted
+//         controls
+//         alt="All the devices"
+//         src="/movie_app_demo.mp4"
+//         ref={videoEl}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Video;
+
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
+
+class Video extends Component {
+  render() {
+    return (
+      <div className="player-wrapper">
+        <ReactPlayer
+          url="movie_app_demo.MP4"
+          width="90%"
+          height="90%"
+          controls={true}
+        />
+      </div>
+    );
+  }
+}
 
 export default Video;
