0ffe62ac57b592937a2341db2d5cc4717f462844
seung <hhwannn2@gmail.com>
2023-09-21 13:17:14 -0500

    update resume and work experience

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,6 +20,7 @@ const About = () => {
             <img src={Me} alt="myimage" />
           </div>
         </div>
+
         <div className={classes.about__content}>
           <div className={classes.about__cards}>
             <article className={classes.about__card}>
@@ -43,24 +44,23 @@ const About = () => {
           <p>
             I'm a versatile full stack developer with over 2 years of hands-on
             experience and a strong commitment to crafting innovative and
-            user-friendly applications <br />
+            user-friendly applications
             <br />
-            My technical expertise shines in full stack development, with a
+            {/* My technical expertise shines in full stack development, with a
             focus on utilizing React, Angular, and the .NET Framework to create
             robust projects. But I bring more than just tech skills to the
             table. My background also includes invaluable lessons from my time
             in the military, instilling qualities like discipline, leadership,
-            and adaptability.
+            and adaptability */}
             <br />
+            My background not only includes technical expertise in various
+            development skills such as React, Angular and C# but also
+            incorporates the discipline, leadership, and adaptability instilled
+            by my time in the military. This dual perspective allows me to
+            approach challenges with a unique and well-rounded perspective
             <br />
-            My background not only includes technical expertise in React,
-            Angular, and the .NET Framework but also incorporates the
-            discipline, leadership, and adaptability instilled by my time in the
-            military. This dual perspective allows me to approach challenges
-            with a unique and well-rounded perspective.
-            <br />
-            <br /> Let's connect and explore how I can contribute my coding
-            prowess and creative problem-solving to your team's success!
+            {/* <br /> Let's connect and explore how I can contribute my coding
+            prowess and creative problem-solving to your team's success! */}
           </p>
           <CTA />
         </div>
